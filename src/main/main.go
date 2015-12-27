99f3f2f0061827486a8d610555db745263004f95
Sachin Philip <sachin.philip@marlabs.com>
2015-12-28 00:30:00 +0530

    simple render

--- src/main/main.go
+++ src/main/main.go
@@ -4,8 +4,12 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"strings"
 	"log"
+	"text/template"
 	)
+
+// these are runservers
 
 type urls struct{
 
@@ -21,11 +25,11 @@ func (this *urls) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	if err == nil {
 		var contentType string
 
-		if string.HasSuffix(path, ".css"){
+		if strings.HasSuffix(path, ".css"){
 			contentType = "text/css"
-		} else if string.HasSuffix(path, "js"){
+		} else if strings.HasSuffix(path, "js"){
 			contentType = "text/js"
-		} else if string.HasSuffix(path, "png"){
+		} else if strings.HasSuffix(path, "png"){
 			contentType = "image/png"
 		} else{
 			contentType = "text/plain"
@@ -36,20 +40,67 @@ func (this *urls) ServeHTTP(w http.ResponseWriter, r *http.Request){
 
 	}  else {
 		w.WriteHeader(404)
-		w.Write([]byte("404" + http.StatusText(404)))
+		w.Write([]byte("404 - Sorry Page Not Found" + http.StatusText(404)))
 	}
 }
 
 
+type Context struct {
+	Title string
+	Desc string
+}
 
 func main() {
 
-	http.Handle("/", new(urls))
-	// http.HandleFunc("/", mainFunc)
+	// http.Handle("/", new(urls))
+	http.HandleFunc("/", mainFunc)
 	http.ListenAndServe(":9090", nil)
 
 }
 
-// func mainFunc(w http.ResponseWriter, req *http.Request){
-// 	w.Write([]byte("Hello World"))
-// }
+func mainFunc(w http.ResponseWriter, req *http.Request){
+	
+	w.Header().Add("Content Type", "text/html")
+	temp, err := template.New("home").Parse(doc)
+
+	if err == nil{
+		context := Context{"todo", "my todo list"}
+		temp.Execute(w, context)
+	}
+
+}
+
+const doc = `
+
+	<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Home | Go</title>
+			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+		</head>
+		<body>
+			<h1>{{.Title}}</h1>
+			<p> {{.Desc}} </p>
+		</body>
+		</html>
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
